23e5c6bf62388848edb6e5d6081a1f41ec19cfdb
RubyQWER <Chanatpong1234@gmail.com>
2023-08-15 14:30:48 +0700

    hw04-7

--- Homework/HW04/hw04-7-Temperator-Convert.cpp
+++ Homework/HW04/hw04-7-Temperator-Convert.cpp
@@ -13,3 +13,16 @@
     Output:
         36 degree Celsius = 96.8 degree Fahrenheit
 */
+#include <stdio.h>
+ int main () {
+    int C ;
+    float F ;
+    //INPUT degree Celsius
+    printf( "User input : " ) ;
+    scanf("%d", &C ) ;
+    //แปลงค่า
+    float ans ;
+    ans =  C * 1.8 +32 ;
+    printf("%d degree Celsius = %.1f degree Fahrenheit", C ,ans);
+    return 0 ;
+ } 
